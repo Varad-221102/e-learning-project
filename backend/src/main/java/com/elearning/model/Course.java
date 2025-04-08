04a82e29dbacfd18915ec97da61b7bfd7400af41
Varad-221102 <varaddhekanecgpi@gmail.com>
2025-04-08 15:45:13 +0530

    Added CourseRepository, CourseService, and CourseController for Course Module

---
+++ backend/src/main/java/com/elearning/model/Course.java
@@ -0,0 +1,31 @@
+package com.elearning.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Course title is mandatory")
+    private String title;
+
+    @NotBlank(message = "Course description is mandatory")
+    private String description;
+
+    private String instructor;
+}
---
+++ backend/src/main/java/com/elearning/service/CourseService.java
@@ -0,0 +1,27 @@
+package com.elearning.service;
+
+import com.elearning.model.Course;
+import com.elearning.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course addCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+}
